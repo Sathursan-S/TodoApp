81e5376d3224c4dafd4eebfeba9f6ab13c117a27
Sathursan-S <sathursan500@gmail.com>
2024-05-20 16:19:33 +0530

    Update launchSettings.json

--- backend/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ backend/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+198e47e7775be497001f6c5227beb42cf06ace3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb0028a6171b73ab8ea1eae8bf58e2976aa4c9bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
