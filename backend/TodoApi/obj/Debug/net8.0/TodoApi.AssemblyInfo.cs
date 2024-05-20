e6e36a74afa5671c9ff6a2d81426300e17d6def7
Sathursan-S <sathursan500@gmail.com>
2024-05-20 18:00:54 +0530

    chore: Update .gitignore to ignore TodoApi assembly info files

--- backend/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ backend/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+391e88519456615fb4b3b29f6a24611a963e4a89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11e725216bbc82626fd8f8bf9024f4e94c2ff1d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
