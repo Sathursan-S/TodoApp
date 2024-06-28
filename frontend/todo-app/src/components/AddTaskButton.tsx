75613cc0beaebd8c3577b280b444f61a5fb7b422
Sathursan-S <sathursan500@gmail.com>
2024-06-28 13:06:01 +0530

    chore: Update AddTaskButton component to use red background color instead of blue

--- frontend/todo-app/src/components/AddTaskButton.tsx
+++ frontend/todo-app/src/components/AddTaskButton.tsx
@@ -10,7 +10,7 @@ const AddTaskButton: React.FC = () => {
   return (
     <div>
       <button
-        className="px-4 py-2 bg-blue-500 text-white rounded-md"
+        className="px-4 py-2 bg-red-500 text-white rounded-md"
         onClick={handleOpenForm}
       >
         Add Task
